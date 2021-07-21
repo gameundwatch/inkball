3ea7f15b41f782713502c543a4ae926bc33d4992
BuildTools <unconfigured@null.spigotmc.org>
2021-07-21 13:55:51 +0900

    changed block drawing

--- inkball/Block.cpp
+++ inkball/Block.cpp
@@ -18,5 +18,16 @@ Block::Block(int x, int y, int i)
 void Block::drawBlock(int x,int y) {
 	Body.setSize(Size.x,Size.y);
 	Body.setPos(40 * x, 40 * y);
-	Body.draw().drawFrame(1,0,ColorF(0.8,0.8,0.8));
+	switch (Type)
+	{
+	case 0:
+		Body.draw(ColorF(0.8, 0.8, 0.8)).drawFrame(1, 0, ColorF(0.5, 0.5, 0.5));
+		break;
+	case 1:
+		Body.draw(Arg::top = ColorF(1, 1, 1),Arg::bottom = ColorF(0.8, 0.8, 0.8)).drawFrame(1, 0, ColorF(0.2, 0.2, 0,2));
+		break;
+	default:
+		break;
+	}
+
 }
